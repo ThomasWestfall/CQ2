e0db548539df45a0f1508b53ffa9fec86e0d3626
ThomasWestfall <thomas27@vt.edu>
2024-04-25 13:54:21 +1000

    okay

--- R/Chat1.R
+++ R/Chat1.R
@@ -1,14 +1,14 @@
 #' Runs simple C-Q model
 #'
-#' \code{Chat1} get simple CQ model
+#' \code{Chat1} run simple CQ model CQ model: \(C=aQ^b\)
 #'
-#' Chat1 runs the simple CQ model: $C=aQ\^b$. This predicts in-stream concentration $C$ based on observed streamflow $Q$ data.
+#' Chat1 runs the simple CQ model: $C=aQ^b$. This predicts in-stream concentration $C$ based on observed streamflow $Q$ data.
 #'
 #' @details
 #' The input requires observed streamflow data on a daily timescale and two parameters, $a$ and $b$.
 #'
 #' @param params are two values for setting parameters $a$ and $b$. The parameters can be a single value or a vector. The calibration procedure runs the model in vector form, calling Chat1 with a vector of guesses for each parameter. The bounds and initial guess are set using \code{getBounds}
-#' Note: $a = 10^params[1]$ and $b = params[2]$
+#' Note: \(a = 10^params[1]\) and \(b = params[2]\)
 #'
 #' @param flow streamflow dataframe. Vector or array with duplicates in each column for model calibration
 #'
